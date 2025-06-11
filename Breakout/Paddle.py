cfef2407d32812f280441e108b336c5b2f4350e3
cwh555 <bergerchen719@gmail.com>
2025-06-11 14:38:18 +0800

    Paddle

--- Breakout/Paddle.py
+++ Breakout/Paddle.py
@@ -2,16 +2,32 @@ import tkinter as tk
 import GameObject
 
 class Paddle(GameObject):
-    def __init__(self, canvas, x, y):
-      '''
-        the shape is rectangle, init __width, __height
-        __ball: Ball
-        init __item
-        call constructor of GameObject
-      '''
+    def __init__(self, canvas, x, y, width = 80, height = 10):
+        '''
+            the shape is rectangle, init __width, __height
+            __ball: Ball
+            init __item
+            call constructor of GameObject
+        '''
+        self.__width = width
+        self.__height = height
+        self.__ball = None
+        item = canvas.create_rectangle(x - width / 2, y - height / 2,
+                                       x + width / 2, y + height / 2,
+                                       fill = 'blue')
+        super().__init__(canvas, item)
 
     def set_ball(self, ball):
-      ''' set the ball  '''
+        ''' set the ball  '''
+        self.__ball = ball
 
     def move(self, offset):
-      ''' move the paddle horizontally'''
+        ''' move the paddle horizontally'''
+        coords = self.getposition()
+        # get the width of canvas
+        width = self.canvas.winfo_width()
+        if coords[0] + offset >= 0 and coords[2] + offset <= width:
+            super().move(offset, 0)
+            if self.__ball is not None:
+                # ball should move with the paddle since the game has not started yet
+                self.__ball.move(offset, 0)
