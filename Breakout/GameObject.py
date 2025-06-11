16b8eb3de1991da761d73eae121fd5336c72f528
cwh555 <bergerchen719@gmail.com>
2025-06-11 14:18:52 +0800

    base class GameObject

--- Breakout/GameObject.py
+++ Breakout/GameObject.py
@@ -2,21 +2,27 @@ import tkinter as tk
 
 class GameObject:
     def __init__(self, canvas, item):
-      '''
-        初始化__canvas, __item
-      '''
+        '''
+            初始化__canvas, __item
+        '''
+        self.__canvas = canvas
+        self.__item = item
+        
     
     def get_position(self):
-      '''
-        return position in canvas
-      '''
+        '''
+            return position in canvas
+        '''
+        return self.__canvas.coords(self.__item)
 
     def move(self, x, y):
-      '''
-        move to (x, y)
-      '''
+        '''
+            move to (x, y)
+        '''
+        self.__canvas.move(self.__item, x, y)
 
     def delete(self):
-      '''
-        delete the item from canvas
-      '''
+        '''
+            delete the item from canvas
+        '''
+        self.__canvas.delete(self.__item)
