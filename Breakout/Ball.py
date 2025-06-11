a7e327f798a21f891b95b231bafab79426374447
cwh555 <bergerchen719@gmail.com>
2025-06-11 14:25:48 +0800

    Ball

--- Breakout/Ball.py
+++ Breakout/Ball.py
@@ -2,14 +2,24 @@ import tkinter as tk
 import GameObject
 
 class Ball(GameObject):
-    def __init__(self, canvas, x, y):
-      '''
-        初始化__radius, __direction[2], __speed, __live, __item
-        call constructor of GameObject
-      '''
+    def __init__(self, canvas, x, y, radius, speed, life):
+        '''
+            初始化__radius, __direction[2], __speed, __life, __item
+            call constructor of GameObject
+        '''
+        self.__radius = radius
+        self.__speed = speed
+        self.__life = live
+        self.__direction = [1, -1]
+        item = canvas.create_oval(x - radius, y - radius, 
+                                         x + radius, y + radius,
+                                         fill = 'white')
+        super().__init(self, canvas, item)
 
     def get_life(self):
-      ''' return life '''
+        ''' return life '''
+        return self.__life
 
     def modify_life(self, delta):
-      ''' __life += delta '''
+        ''' __life += delta '''
+        self.__life += delta
