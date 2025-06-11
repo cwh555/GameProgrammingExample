8732302fd060def24760e5bff758ff3f9b7479d0
cwh555 <bergerchen719@gmail.com>
2025-06-11 14:28:11 +0800

    modify Ball.__init__

--- Breakout/Ball.py
+++ Breakout/Ball.py
@@ -2,14 +2,14 @@ import tkinter as tk
 import GameObject
 
 class Ball(GameObject):
-    def __init__(self, canvas, x, y, radius, speed, life):
+    def __init__(self, canvas, x, y, radius = 10, speed = 10, life = 3):
         '''
             初始化__radius, __direction[2], __speed, __life, __item
             call constructor of GameObject
         '''
         self.__radius = radius
         self.__speed = speed
-        self.__life = live
+        self.__life = life
         self.__direction = [1, -1]
         item = canvas.create_oval(x - radius, y - radius, 
                                          x + radius, y + radius,
