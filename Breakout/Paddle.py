cafcb30722d34ecec738e7e8ced37f24c29096a6
cwh555 <bergerchen719@gmail.com>
2025-06-11 16:40:55 +0800

    make paddle move smoother

--- Breakout/Paddle.py
+++ Breakout/Paddle.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from GameObject import GameObject
+import time
 
 class Paddle(GameObject):
     def __init__(self, canvas, x, y, width = 80, height = 10):
@@ -12,14 +13,19 @@ class Paddle(GameObject):
         self._width = width
         self._height = height
         self._ball = None
+        self._moving = False
         item = canvas.create_rectangle(x - width / 2, y - height / 2,
                                        x + width / 2, y + height / 2,
                                        fill = 'blue', tags = 'paddle')
         super().__init__(canvas, item)
-        self._canvas.bind('<Left>',
-                          lambda _: self.move(-10))
-        self._canvas.bind('<Right>',
-                          lambda _: self.move(10))
+        self._canvas.bind("<KeyPress-Left>",
+                          lambda _: self._action(-1))
+        self._canvas.bind("<KeyRelease-Left>",
+                          lambda _: setattr(self, '_moving', False))
+        self._canvas.bind("<KeyPress-Right>",
+                          lambda _: self._action(1))
+        self._canvas.bind("<KeyRelease-Right>",
+                          lambda _: setattr(self, '_moving', False))
 
     def set_ball(self, ball):
         ''' set the ball  '''
@@ -38,3 +44,19 @@ class Paddle(GameObject):
 
     def get_item(self):
         return self._item
+    
+    def _set_time(self) -> None:
+        self._time = time.time()
+
+    def _get_time(self) -> float:
+        return time.time() - self._time
+    
+    def _action(self, direction: int) -> None:
+        self._moving = True
+        self._set_time()
+        self._continue_moving(direction)
+
+    def _continue_moving(self, direction: int) -> None:
+        if self._moving:
+            self.move(10 * direction * (self._get_time() + 0.5))
+            self._canvas.after(50, lambda: self._continue_moving(direction))
